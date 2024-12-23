08d6f9b6c2c064d16aa4e206c64ce92bfe264390
Logan Kudlacik <90789169+logankud@users.noreply.github.com>
2024-12-23 14:23:16 +0000

    Automatic commit before Assistant edits

---
+++ finetune-llm/setup.sh
@@ -0,0 +1,10 @@
+python -m venv venv
+
+# Install requirements
+pip install -r finetune-llm/requirements.txt
+
+# Activate venv
+source venv/bin/activate 
+
+# Launch jupyter notebook
+jupyter notebook --ip=0.0.0.0 --port=8050 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
